6a2983df5bf77d9c78287de91e8682765bc7157e
vadimopanasenko <vadimopanasenka1@gmail.com>
2024-10-04 23:03:14 +0200

    hm 3.2

---
+++ lesson.py
@@ -0,0 +1,15 @@
+num1 = [42,7,5,37]
+num2 = [5]
+num3 = []
+num4 = [235,867,436,965,345]
+#Задал числа
+result1 = [num1[3], num1[0], num1[1], num1[2]]
+result2 = [num2[0]] #Or print(num2)
+result3 = []
+result4 = [num4[1],num4[2],num4[3],num4[4],num4[0]]
+#Вставил индексы для правильно расположения чисел
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+#Вывел результат
